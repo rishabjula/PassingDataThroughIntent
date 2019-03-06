bd6bf340564324d364599e94922248dc46d95fec
Rishab Jula <rishab.jula@gmail.com>
2019-03-06 23:04:30 +0530

    updated

--- app/src/main/java/com/tripod/passingdatathroughintent/SecondActivity.java
+++ app/src/main/java/com/tripod/passingdatathroughintent/SecondActivity.java
@@ -3,16 +3,20 @@ package com.tripod.passingdatathroughintent;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
  TextView t1;
+ Button gb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
         t1 = findViewById(R.id.textView);
+        gb = findViewById(R.id.gobackbutton);
         Bundle message = getIntent().getExtras();
 
         if(message!=null)
@@ -23,5 +27,15 @@ public class SecondActivity extends AppCompatActivity {
             t1.setText(data+intValue);
 
         }
+
+        gb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentReturn = getIntent();
+                intentReturn.putExtra("DataReturn"," data from SecondActivity");
+                setResult(RESULT_OK,intentReturn);
+                finish();
+            }
+        });
     }
 }
